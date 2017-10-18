a4dbacbed62a4f14b388d946964e1fac30b7b9fb
Sayan Riju Chakrabarti <s26c.sayan@gmail.com>
2017-10-18 17:43:01 +0530

    Wed Oct 18 17:43:01 IST 2017

---
+++ data.js
@@ -0,0 +1,24 @@
+const drivers = [
+  { id: 1, lat: 43.1839958, lng: 16.5992505, name: "Celine Georgeot" },
+  { id: 2, lat: -2.8534379, lng: 108.2941031, name: "Thurston Brayfield" },
+  { id: 3, lat: 56.0518037, lng: 92.9241062, name: "Megan Farlane" },
+  { id: 4, lat: 26.433417, lng: 110.856622, name: "Godwin Lowater" },
+  { id: 5, lat: 50.6276718, lng: 81.911898, name: "Randolf Eborall" },
+  { id: 6, lat: 45.0818466, lng: 18.3957811, name: "Karlik Brotherton" },
+  { id: 7, lat: 41.0938736, lng: 141.3941151, name: "Randi Bilsland" },
+  { id: 8, lat: 51.30568, lng: 20.1143155, name: "Grethel Happer" },
+  { id: 9, lat: 35.580629, lng: 104.610668, name: "Catlin Freschi" },
+  { id: 10, lat: 31.830362, lng: 119.97167, name: "Kate Carlsson" },
+  { id: 11, lat: 52.5170826, lng: 13.3236611, name: "Kalila Zold" },
+  { id: 12, lat: 47.0171563, lng: -68.1430273, name: "Bowie Yanshinov" },
+  { id: 13, lat: -21.6029893, lng: -48.3644226, name: "Almeria Durnford" },
+  { id: 14, lat: 46.5641539, lng: 16.443954, name: "Goran Duggary" },
+  { id: 15, lat: 40.9121933, lng: -8.4162288, name: "Lela Illing" },
+  { id: 16, lat: 0.2826569, lng: 32.5662219, name: "Codi Ezzy" },
+  { id: 17, lat: 9.4387872, lng: -70.490794, name: "Leia Sevior" },
+  { id: 18, lat: 17.8200009, lng: 120.4544261, name: "Pall Burditt" },
+  { id: 19, lat: 18.1530215, lng: -77.471452, name: "Malvin Magne" },
+  { id: 20, lat: -17.3542865, lng: -70.1331038, name: "Fianna Delamere" }
+]
+
+module.export = drivers
--- index.js
+++ index.js
@@ -3,20 +3,27 @@ const Faye = require("faye")
 const Redis = require("redis")
 const express = require("express")
 
+const gmap = require("@google/maps").createClient({
+  key: process.env.GOOGLE_MAPS_API_KEY || "AIzaSyBfXwliPdMdFFkPNRGQByETe4sn-x06X_4"
+});
+
+const drivers = require("./data")
+
 const redis = Redis.createClient()
 redis.on("error", err => console.log(`REDIS Error: ${err.message}`))
 
 const app = express()
-const server = http.createServer(app)
+app.set("view engine", "ejs")
 
+const server = http.createServer(app)
 const bayeux = new Faye.NodeAdapter({ mount: "/" })
 bayeux.attach(server)
 
-const faye = new Faye.Client("http://localhost:8000")
+const faye = new Faye.Client("http://localhost:3000/")
 
 function setLiveLocation(arg) {
-  redis.set(`lng:${arg.clientId}`, arg.lng, "EX", 10)
-  redis.set(`lat:${arg.clientId}`, arg.lat, "EX", 10)
+  redis.set(`lat:${arg.id}`, arg.lat, "EX", 300) // becomes idle in 5 mins
+  redis.set(`lng:${arg.id}`, arg.lng, "EX", 300) // becomes idle in 5 mins
 }
 
 faye.subscribe("/locupdate", setLiveLocation)
@@ -25,8 +32,38 @@ app.post("/locupdate", (req, res) => {
   res.send("OK")  // no waiting
 })
 
-app.get("/near/:range/:lng?/:lat?", (res, res) => {
-  
+app.get("/", (req, res) => {
+  console.log("welcome........");
+  res.render("whereami")
+})
+app.get("/within/:range/:lng/:lat", (req, res) => {
+  const rQuery = []
+  drivers.forEach((driver) => {
+    rQuery.push(`lat:${driver.id}`)
+    rQuery.push(`lng:${driver.id}`)
+  })
+  redis.mget(rQuery, (rError, rResponse) => {
+    if (!rError) {
+      const gQuery = {
+        origins: [{ lat: req.params.lat, lng: req.params.lng }],
+        destinations: []
+      }
+      for (let i = 0; i < rResponse.length; i += 2) {
+        gQuery.destinations.push({ lat: rResponse[i], lng: rResponse[i + 1] })
+      }
+      gmap.distanceMatrix(gQuery, (gError, gResponse) => {
+        if (!gError) {
+          res.send(gResponse.json.results)
+        } else {
+          console.log("!!!!gError: ", gError);
+          res.send({ error: true, reason: gError.message })
+        }
+      })
+    } else {
+      console.log("!!!!rError: ", rError);
+      res.send({ error: true, reason: rError.message })      
+    }
+  });
 })
 
-server.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000)
